88d42bf5a72b46395030ff7884d5bda801d96702
Mark Sharp <mark.sharp@solent.ac.uk>
2023-05-10 09:29:41 +0100

    Remove default from XML definition of assessmentname

--- db/upgrade.php
+++ db/upgrade.php
@@ -196,7 +196,7 @@ function xmldb_local_solsits_upgrade($oldversion) {
         $dbman->add_key($table, $key);
 
         $table = new xmldb_table('local_solsits_assign');
-        $field = new xmldb_field('assessmentname', XMLDB_TYPE_CHAR, 100, null, null, false, '', 'assessmentcode');
+        $field = new xmldb_field('assessmentname', XMLDB_TYPE_CHAR, 100, null, null, false, null, 'assessmentcode');
         if (!$dbman->field_exists($table, $field)) {
             $dbman->add_field($table, $field);
         }

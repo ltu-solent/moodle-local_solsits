00fc352b26e62b022f9db3c357a27373f41fc968
Mark Sharp <mark.sharp@solent.ac.uk>
2025-04-30 10:50:55 +0100

    split assignment intro according to role

--- classes/local/shortcodes.php
+++ classes/local/shortcodes.php
@@ -127,16 +127,41 @@ class shortcodes {
      * @return string The new content.
      */
     public static function assignmentintro($shortcode, $args, $content, $env, $next) {
-        global $DB, $OUTPUT, $USER;
+        global $OUTPUT;
         $config = get_config('local_solsits');
         $context = $env->context;
-        $now = time();
         $data = new stdClass();
-
         $sitsassign = sitsassign::get_record(['cmid' => $context->instanceid]);
         if (!$sitsassign) {
             return '';
         }
+        // Note: Side admins will have both.
+        $isstudent = has_capability('mod/assign:submit', $context);
+        if ($isstudent) {
+            $data = self::studentintro($sitsassign, $context, $config);
+            $data->hasstudentinfo = true;
+        }
+
+        $istutor = has_capability('mod/assign:grade', $context);
+        if ($istutor) {
+            $data->tutorinfo = self::tutorintro($sitsassign, $context, $config);
+            $data->hastutorinfo = count($data->tutorinfo) > 0;
+        }
+
+        return $OUTPUT->render_from_template('local_solsits/assignmentintro', $data);
+    }
+
+    /**
+     * Adds assignment info for students about their submission
+     *
+     * @param sitsassign $sitsassign
+     * @param context $context
+     * @param stdClass $config
+     * @return stdClass
+     */
+    private static function studentintro($sitsassign, $context, $config): stdClass {
+        global $DB, $USER;
+        $data = new stdClass();
         $data->style = 'solent-sea-blue';
         $coursecontext = $context->get_course_context();
         [$course, $cm] = get_course_and_cm_from_cmid($sitsassign->get('cmid'), 'assign');
@@ -160,10 +185,15 @@ class shortcodes {
             'assignment' => $cm->instance,
             'latest' => 1,
         ]);
-        if (!$submission) {
-            $data->status = get_string('notsubmitted', 'local_solsits');
-        } else {
+        $submissionplugins = $assign->is_any_submission_plugin_enabled();
+        if ($submission && $submission->status != ASSIGN_SUBMISSION_STATUS_NEW) {
             $data->status = get_string('submissionstatus_' . $submission->status, 'assign');
+        } else {
+            if (!$submissionplugins) {
+                $data->status = get_string('noonlinesubmissions', 'assign');
+            } else {
+                get_string('noattempt', 'assign');
+            }
         }
 
         $submittedstatuses = ['submitted', 'marked'];
@@ -189,49 +219,62 @@ class shortcodes {
                 'url' => $briefcm->get_url(),
             ];
         }
-        if (has_capability('mod/assign:grade', $context)) {
-            $tutorinfo = [];
-            if ($duedate > $now) {
-                $mailinglist = get_config('local_solsits', 'assignmentduedatechange_mailinglist') ?? '';
-                if (empty($mailinglist)) {
-                    $tutorinfo[] = get_string('checkduedatenomessage', 'local_solsits', [
-                        'duedate' => userdate($duedate, $strftimedatetimeaccurate),
-                    ]);
-                } else {
-                    $tutorinfo[] = get_string('checkduedate', 'local_solsits', [
-                        'duedate' => userdate($duedate, $strftimedatetimeaccurate),
-                        'sitsref' => $sitsassign->get('sitsref'),
-                        'title' => $sitsassign->get('title'),
-                        'cmid' => $sitsassign->get('cmid'),
-                    ]);
-                }
+        return $data;
+    }
+
+    /**
+     * Adds assignment set-up info for tutors
+     *
+     * @param sitsassign $sitsassign
+     * @param context $context
+     * @param stdClass $config
+     * @return string[]
+     */
+    private static function tutorintro($sitsassign, $context, $config): array {
+        $now = time();
+        $tutorinfo = [];
+        [$course, $cm] = get_course_and_cm_from_cmid($sitsassign->get('cmid'), 'assign');
+        $strftimedatetimeaccurate = '%d %B %Y, %I:%M:%S %p';
+        $assign = new assign($context, $cm, $course);
+        $duedate = $assign->get_instance()->duedate;
+        if ($duedate > $now) {
+            $mailinglist = get_config('local_solsits', 'assignmentduedatechange_mailinglist') ?? '';
+            if (empty($mailinglist)) {
+                $tutorinfo[] = get_string('checkduedatenomessage', 'local_solsits', [
+                    'duedate' => userdate($duedate, $strftimedatetimeaccurate),
+                ]);
+            } else {
+                $tutorinfo[] = get_string('checkduedate', 'local_solsits', [
+                    'duedate' => userdate($duedate, $strftimedatetimeaccurate),
+                    'sitsref' => $sitsassign->get('sitsref'),
+                    'title' => $sitsassign->get('title'),
+                    'cmid' => $sitsassign->get('cmid'),
+                ]);
             }
-            if (!$cm->visible) {
-                $tutorinfo[] = get_string('assignmentnotvisible', 'local_solsits');
+        }
+        if (!$cm->visible) {
+            $tutorinfo[] = get_string('assignmentnotvisible', 'local_solsits');
+        }
+        $submissionplugins = $assign->is_any_submission_plugin_enabled();
+        if (!$submissionplugins) {
+            $tutorinfo[] = get_string('nosubmissionplugins', 'local_solsits');
+        }
+        $gradingdue = $assign->get_instance()->gradingduedate;
+        if ($now > $duedate && $now < $gradingdue) {
+            $tutorinfo[] = get_string('gradingdueby', 'local_solsits', userdate($gradingdue, $strftimedatetimeaccurate));
+        }
+        if ($cm->availability != null) {
+            if ($sitsassign->get('reattempt') > 0) {
+                $groupslink = html_writer::link(
+                    new moodle_url('/group/index.php', ['id' => $sitsassign->get('courseid')]),
+                    $sitsassign->get('title')
+                );
+                $tutorinfo[] = get_string('reattemptavailability', 'local_solsits', $groupslink);
+            } else {
+                $tutorinfo[] = get_string('availabilityconditions', 'local_solsits');
             }
-            $submissionplugins = $assign->is_any_submission_plugin_enabled();
-            if (!$submissionplugins) {
-                $tutorinfo[] = get_string('nosubmissionplugins', 'local_solsits');
-            }
-            $gradingdue = $assign->get_instance()->gradingduedate;
-            if ($now > $duedate && $now < $gradingdue) {
-                $tutorinfo[] = get_string('gradingdueby', 'local_solsits', userdate($gradingdue, $strftimedatetimeaccurate));
-            }
-            if ($cm->availability != null) {
-                if ($sitsassign->get('reattempt') > 0) {
-                    $groupslink = html_writer::link(
-                        new moodle_url('/group/index.php', ['id' => $sitsassign->get('courseid')]),
-                        $sitsassign->get('title')
-                    );
-                    $tutorinfo[] = get_string('reattemptavailability', 'local_solsits', $groupslink);
-                } else {
-                    $tutorinfo[] = get_string('availabilityconditions', 'local_solsits');
-                }
-            }
-            $data->hastutorinfo = count($tutorinfo) > 0;
-            $data->tutorinfo = $tutorinfo;
         }
 
-        return $OUTPUT->render_from_template('local_solsits/assignmentintro', $data);
+        return $tutorinfo;
     }
 }

cdbe788365b8961d3c6e34354379762be6eebf22
Mark Sharp <mark.sharp@solent.ac.uk>
2025-07-28 12:20:41 +0100

    Version bump

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025032803;
+$plugin->version   = 2025032804;
 $plugin->release   = 'v4.5-r1';
 $plugin->requires  = 2024100700;
 $plugin->component = 'local_solsits';
